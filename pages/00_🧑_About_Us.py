760c2d0316a69ffbc8345947da7f08dfd71434cf
Aditya Wardianto <83759122+ditw11mhs@users.noreply.github.com>
2022-07-23 14:58:29 +0700

    fix: fix link in all contacts

--- pages/00_🧑_About_Us.py
+++ pages/00_🧑_About_Us.py
@@ -27,7 +27,7 @@ def about_speaker(adit_circ):
     - Aditya Wardianto or Adit is a Final Year undergraduate student from Biomedical Engineering Department Sepuluh Nopember Institute of Technology.\n
     - He has used Python for 3+ years to freelance and is currently freelancing as a machine learning engineer at PT Jokeen Solusi Teknologi.\n
     - He is a Certified Tensorflow Developer thanks to Bangkit Academy led by Google, GoTo, and Traveloka.
-    - Contact me through [WA]("https://wa.me/6281329968144") - [Linkedin]("https://www.linkedin.com/in/adityawardianto/") - [Github](https://github.com/ditw11mhs)
+    - Contact me through [WA](https://wa.me/6281329968144) - [Linkedin](https://www.linkedin.com/in/adityawardianto) - [Github](https://github.com/ditw11mhs)
     """
     )
 
@@ -42,7 +42,7 @@ def about_jokeen(jokeen_logo):
         - Jokeen.id is a start-up in the field of engineering that was established on February 7, 2020.\n
         - Jokeen.id provides services ranging from and not restricted to mechanical product development, engineering design work, manufacture, to mass production of mechatronic products.\n
         - In our work, we uphold client satisfaction by prioritizing professionalism and punctuality.\n
-        - Contact Us through [WA]("https://wa.me/6281357172537") - [Linkedin]("https://www.linkedin.com/company/jokeen-id/") - [Instagram](https://www.instagram.com/jokeen.id/) 
+        - Contact Us through [WA](https://wa.me/6281357172537) - [Linkedin](https://www.linkedin.com/company/jokeen-id) - [Instagram](https://www.instagram.com/jokeen.id) 
         """
     )
 
@@ -57,7 +57,7 @@ def about_eng_con(engineer_connect_logo):
         - Engineer Connect's services range from Engineer Group Study, Skill Workshop, and On-Demand Tutor.\n
         - Engineer Connect is also supported and part of the Leadership Project of Young Leaders of Indonesia by McKinsey & Company.\n
         - Engineer Connect will always strive to provide the best service quality and effectively connect Indonesia's engineering students.
-        - Contact us through [Linkedin]("https://www.linkedin.com/company/engineerconnect/") - [Instagram](https://www.instagram.com/engineerconnect/)
+        - Contact us through [Linkedin](https://www.linkedin.com/company/engineerconnect) - [Instagram](https://www.instagram.com/engineerconnect)
         """
     )
 
